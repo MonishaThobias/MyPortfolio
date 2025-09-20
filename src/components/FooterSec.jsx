5be214716987add219845e09d394458c0de3ec29
MonishaThobias <monishaklientas@gmail.com>
2025-09-20 15:15:35 +0530

    Scroll to top button

--- src/components/FooterSec.jsx
+++ src/components/FooterSec.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const FooterSec = () => {
+  const [showButton, setShowButton] = useState(false)
+   useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowButton(true)
+      } else {
+        setShowButton(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
   return (
     <>
        {/* FOOTER */}
@@ -8,6 +29,16 @@ const FooterSec = () => {
         <div className="mx-auto max-w-6xl items-center px-4 py-8 text-sm text-gray-400 flex justify-center h-16 ">
           © {new Date().getFullYear()} Monisha — Built with React & Tailwind
         </div>
+         {showButton && (
+          <button
+            onClick={scrollToTop}
+            className="fixed bottom-6 right-6 bg-gray-800 text-white p-3 rounded-full shadow-lg hover:bg-gray-700 transition"
+            aria-label="Scroll to Top"
+            title='Scroll to Top'
+          >
+            ↑
+          </button>
+        )}
       </footer>
     </>
   )
